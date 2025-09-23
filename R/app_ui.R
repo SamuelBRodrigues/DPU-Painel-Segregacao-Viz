64b7303ee27302ff8b5f0b2c4d320b265fec4bf6
adelino-dev <adelino.developer@gmail.com>
2025-09-23 11:44:17 -0300

    :sparkles: feat: add responsiviness to the footer

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,38 +1,44 @@
-#' Application UI
-#'
-#' @noRd
-#' @import shiny
-#' @import bslib
 app_ui <- function() {
+
+  # CSS
+  css_footer <- system.file("app/www/footer.css", package = "dpupsviz")
+  css_apresentacao <- system.file("app/www/apresentacao.css", package = "dpupsviz")
+
+  #Page Navbar
   bslib::page_navbar(
     title = "Painel de Segregação Étnico-Racial",
-    footer = shiny::tags$div(
-      style = "background-color: #5BB02F; display:flex; align-items:flex-start; gap:10px; width = 100vw; padding: 10px;",
-      shiny::tags$img(src = "www/logo_DPU.png", height = "100px"),
-      shiny::tags$img(src = "https://shiny-server.dpu.def.br/catadores.html/DPU_PNUD.png", height = "100px")
-    ),
     window_title = "Painel Segregação Étnico-Racial",
-    theme = bslib::bs_theme(version = 5,
-                            bg = "#FFFFFF",
-                            fg = "#000000ff",
-                            base_font = bslib::font_collection(
-                              bslib::font_google("Lato"),
-                              "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'"
-                              )
-                            ),
-    bslib::nav_panel("Apresentação",
-                     bslib::card(
-                       bslib::card_body(
-                         tagList(
-                           # Carrega o CSS externo
-                           includeCSS(system.file("app/www/apresentacao.css", package = "dpupsviz")),
-                           # Inclui o HTML
-                           shiny::includeHTML(
-                             system.file("app/www/apresentacao.html", package = "dpupsviz")
-                           )
-                         )
-                       )
-                     )
+
+    # Tema
+    theme = bslib::bs_theme(
+      version = 5,
+      bg = "#FFFFFF",
+      fg = "#000000ff",
+      base_font = bslib::font_collection(
+        bslib::font_google("Lato"),
+        "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'"
+      )
+    ),
+
+    # Inclui os CSS via includeCSS()
+    shiny::includeCSS(css_footer),
+    shiny::includeCSS(css_apresentacao),
+
+    # Footer
+    footer = shiny::tags$div(
+      class = "footer",
+      shiny::tags$img(src = "www/logo_DPU.png"),
+      shiny::tags$img(src = "https://shiny-server.dpu.def.br/catadores.html/DPU_PNUD.png")
+    ),
+
+    # Painéis
+    bslib::nav_panel(
+      "Apresentação",
+      bslib::card(
+        bslib::card_body(
+          shiny::includeHTML(system.file("app/www/apresentacao.html", package = "dpupsviz"))
+        )
+      )
     ),
     bslib::nav_panel("Índices de Segregação", mod_tabela_ui("tabela")),
     bslib::nav_panel("Mapa", mod_mapa_ui("mapa"))
